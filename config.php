98fc50dfb8c8dea45f075daa3da4090ec282aef1
Jake Cruse <jdcruse66@live.com>
2020-07-30 10:10:32 +0100

    Installed Jigsaw

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'production' => false,
+    'baseUrl' => '',
+    'title' => 'Jake Cruse | Developer',
+    'description' => 'Website description.',
+    'collections' => [],
+];
